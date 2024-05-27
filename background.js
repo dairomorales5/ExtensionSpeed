34126c5f5ae332eae17217ad7935d84cd71cb7bc
dairomorales5 <damoralesj@unal.edu.co>
2024-05-27 15:14:10 -0500

    Soportar comandos de velocidad predefinidos

--- background.js
+++ background.js
@@ -5,7 +5,10 @@ chrome.runtime.onInstalled.addListener(() => {
 });
 
 // Función para disparar la ejecución de la extensión
-function speed() {
+function speed(spd) {
+  if (spd) {
+      chrome.storage.sync.set({speed: spd});
+  }
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
       tabs.forEach(function (tab) {
           chrome.scripting.executeScript({
@@ -21,6 +24,12 @@ chrome.commands.onCommand.addListener(function (command) {
     switch (command) {
         case 'restart-speed':
             speed();
+            break;
+        case "half-speed":
+            speed('0.5');
+            break;
+        case "max-speed":
+            speed('3');
             break;
         default:
             console.log(`Command ${command} not found`);
